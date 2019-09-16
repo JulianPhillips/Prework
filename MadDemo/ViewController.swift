7724cd77531949af1d73b846fcd3d88d88990874
Julian Phillips Parker <Julian.park1988@gmail.com>
2019-09-15 22:30:16 -0400

    Initial Commit

---
+++ MadDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MadDemo
+//
+//  Created by Julian Parker on 9/15/19.
+//  Copyright © 2019 Jawny Jules. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
