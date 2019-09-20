d1f37230270cc4f206e4821f7febf9f12d8061ad
Julian Phillips Parker <Julian.park1988@gmail.com>
2019-09-20 10:43:32 -0400

    update

--- MadDemo/ViewController.swift
+++ MadDemo/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet var backgroundView: UIView!
+    
+    var defaultString: String = "Type something above!"
     override func viewDidLoad() {
         super.viewDidLoad()
+        backgroundView.backgroundColor = UIColor.purple
+        textLabel.text = defaultString
         // Do any additional setup after loading the view.
     }
 
-
+   
+    
+    @IBAction func updateText(_ sender: Any) {
+        if(textField.text?.count == 0){
+            textLabel.text = defaultString
+        }
+        else{
+     textLabel.text =   textField.text
+        }
+    }
+    
+    @IBAction func changeTextColor(_ sender: Any) {
+        
+        if(textLabel.textColor != UIColor.red){
+        textLabel.textColor = UIColor.red
+        }
+        else{
+        textLabel.textColor = UIColor.black
+        }
+        
+    }
+    
+    @IBAction func changeViewColor(_ sender: Any) {
+        if(backgroundView.backgroundColor != UIColor.red){
+        backgroundView.backgroundColor = UIColor.red
+        }
+        else{
+        backgroundView.backgroundColor = UIColor.yellow
+        }
+    }
+    
+    @IBAction func resetEverything(_ sender: Any) {
+        
+        backgroundView.backgroundColor = UIColor.purple
+        textLabel.text = defaultString
+        textField.text = ""
+        
+    }
+    
 }
 
